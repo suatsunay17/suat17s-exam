27d7524bb86c7816116a4a430f1b630396543a5d
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2023-12-16 14:24:32 +0200

    change urls for ui tests with the deployed project url

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('http://localhost:8080/Shopping-list');
+    await page.goto('https://suat17s-exam.onrender.com/Shopping-list');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
